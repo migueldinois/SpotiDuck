c96ed666239da55f843fe5f7d8c213ff89bf2a68
miuueldinois <miguel.dinois@aluno.senai.br>
2026-02-12 14:12:27 -0300

    Commit Inicial, estrutura de pastas e arquivos htmls, servidor ja rodando

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    return render_template('principal.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('administracao.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=8080, debug=True)
