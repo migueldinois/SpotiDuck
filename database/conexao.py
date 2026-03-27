11e92d47554e93e5ec0ced8a2fedd10f8831b67c
Miguel Lima Dinois <miguel.dinois@aluno.senai.br>
2026-03-27 15:40:51 -0300

    Implement connection type handling in Conexao class
    Add conditional connection handling based on connection type.

--- database/conexao.py
+++ database/conexao.py
@@ -1,16 +1,22 @@
 import mysql.connector
-
+tipo_conexao = "NUVEM"
 class Conexao():
-    
     @staticmethod
     def conectar():
-        conexao = mysql.connector.connect(
-        host="localhost",
-        port=3306,
-        user="root",
-        password="root",
-        database="spotiduck"
-    )
+        if tipo_conexao == "NUVEM":
+            conexao = mysql.connector.connect(
+            host="localhost",
+            port=3306,
+            user="root",
+            password="root",
+            database="spotiduck")
+        else:
+            conexao = mysql.connector.connect(
+            host="mysql-16a29790-spotiduck.j.aivencloud.com",
+            port=15558,
+            user="avnadmin",
+            password="AVNS_vnjO12YffaAjPXcHpC0",
+            database="SpotiDuck")
 
         # Vai entregar coluna nome é esse valor, ao inves de dar tudo separado em ma lista sem direcionamento 
         cursor = conexao.cursor(dictionary=True)
